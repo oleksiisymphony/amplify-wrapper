14f15c11f07111567f1c8e1bac2b6c956a77e742
oleksiisymphony <oleksii.razumovskyi@symphony-solutions.com>
2021-10-26 17:30:09 +0300

    added auth

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
---
+++ webpack.dev.config.ts
@@ -0,0 +1,53 @@
+import path from "path";
+import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config: Configuration = {
+  mode: "development",
+  output: {
+    publicPath: "/",
+  },
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false
+        }
+      },
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new HotModuleReplacementPlugin(),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    historyApiFallback: true,
+    port: 4000,
+    open: true,
+    hot: true
+  },
+};
+
+export default config;
